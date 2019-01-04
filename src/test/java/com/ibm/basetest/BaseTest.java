88e712dd30c8f08154df24760ae04bc0532ab04b
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2019-01-04 15:52:03 +0530

    third commit

--- src/test/java/com/ibm/basetest/BaseTest.java
+++ src/test/java/com/ibm/basetest/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -20,7 +21,7 @@ public class BaseTest {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
 		System.setProperty("webdriver.ie.driver", "./drivers/IEDriverServer.exe");
-		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new InternetExplorerDriver();
 		WebDriverWait wait = new WebDriverWait(driver, 60); // 60 to 90
 
 		driver.manage().window().maximize();
